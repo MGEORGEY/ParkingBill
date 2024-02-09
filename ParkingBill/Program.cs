d8bee943cf59bbfef7c4260872cdccb2e9cb4646
MGEORGEY <mutwirigeorge3@gmail.com>
2024-02-09 10:45:47 +0300

    Add project files.

---
+++ ParkingBill/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ParkingBill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Solution("10:00", "13:21"));
+            Console.WriteLine(Solution("09:42", "11:42"));
+        }
+
+        public static int Solution(string E, string L)
+        {
+            var entryTime = DateTime.Parse(E);
+
+            var exitTime = DateTime.Parse(L);
+
+            var firstHourAssumed = entryTime.AddHours(1);
+
+            var hourTimeDifference = exitTime - firstHourAssumed;
+            if (hourTimeDifference.Minutes > 0)
+                hourTimeDifference = hourTimeDifference.Add(new TimeSpan(1, 0, 0));
+
+            var total = 2 + 3 + (hourTimeDifference.Hours * 4);
+
+            return total;
+        }
+    }
+}
